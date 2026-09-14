73bfd61df5253c2193ff5082fdbff2af014ae9a9
sohaib <sohaibakhtarpapermill@gmail.com>
2026-09-14 15:33:10 +0530

    Secure Django secret key

--- campus_lost_found/settings.py
+++ campus_lost_found/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ziy2#p$d@zt5(c10!m%t370)ck-y*8azf%^%x%4dvn3i57&aak'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
